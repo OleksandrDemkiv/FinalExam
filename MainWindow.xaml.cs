01064a26db65eb7e475af9547d465a8f404eb979
OleksandrDemkiv <s00251154@atu.ie>
2025-01-08 13:46:05 +0000

    booking implemented

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,7 +17,28 @@ namespace FinalExam // https://github.com/OleksandrDemkiv/FinalExam
     /// </summary>
     public partial class MainWindow : Window
     {
-        static List<Event> events = new List<Event>();
+        // all the events and tickets
+        static List<Event> events = new List<Event>
+        {
+            new Event(
+                "Oasis Croke Park", new DateTime(2025, 06, 20), EventType.Music,
+                new List<Ticket>
+                {
+                    new Ticket("Early Bird", 100m, 100),
+                    new VIPTicket("Ticket and Hotel Package", 150m, 100, "4* hotel", 100m),
+                }
+            ),
+
+            new Event(
+                "Electric Picnic", new DateTime(2025, 08, 20), EventType.Music,
+                new List<Ticket>
+                {
+                    new Ticket("Platinium", 150m, 200),
+                    new VIPTicket("Weekend Ticket", 200m, 100, "with camping", 100m)
+
+                }
+            )
+        };
 
         public MainWindow()
         {
@@ -26,35 +47,49 @@ namespace FinalExam // https://github.com/OleksandrDemkiv/FinalExam
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            events = new List<Event>
-            {
-                new Event(
-                    "Oasis Croke Park", new DateTime(2025, 06, 20), EventType.Music,
-                    new List<Ticket>
-                    {
-                        new Ticket("Early Bird", 100m, 100),
-                        new VIPTicket("Ticket and Hotel Package", 150m, 100, "4* hotel", 100m),
-                    }
-                ),
-
-                new Event(
-                    "Electric Picnic", new DateTime(2025, 08, 20), EventType.Music,
-                    new List<Ticket>
-                    {
-                        new Ticket("Platinium", 150m, 200),
-                        new VIPTicket("Weekend Ticket", 200m, 100, "with camping", 100m)
-
-                    }
-                )
-            };
-
+            // set events item source
             EventsBox.ItemsSource = events;
         }
 
         private void EventsBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // when event is selected, set tickets item source
             TicketsBox.ItemsSource = (EventsBox.SelectedItem as Event).Tickets;
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e) // when book button is clicked
+        {
+            int noOfTickets;
+
+            // check for number of tickets
+            if (!int.TryParse(NoOfTickets.Text, out noOfTickets) || noOfTickets <= 0)
+            {
+                MsgLabel.Content = "Please enter a valid number of tickets";
+                MsgLabel.Foreground = Brushes.Red;
+                return;
+            }
+
+            // check for selected event and ticket
+            if (EventsBox.SelectedItem == null || TicketsBox.SelectedItem == null)
+            {
+                MsgLabel.Content = "Please select an event and ticket type";
+                MsgLabel.Foreground = Brushes.Red;
+                return;
+            }
+
+            // check for available tickets
+            if ((TicketsBox.SelectedItem as Ticket).AvailableTickets < noOfTickets)
+            {
+                MsgLabel.Content = "Not enough tickets available";
+                MsgLabel.Foreground = Brushes.Red;
+                return;
+            }
+
+            // if everything is ok, proceed with booking
+            // update available tickets and display message
+            (TicketsBox.SelectedItem as Ticket).AvailableTickets -= noOfTickets;
+            MsgLabel.Content = $"{(TicketsBox.SelectedItem as Ticket).Name}, {noOfTickets}\ntickets booked successfully";
+            MsgLabel.Foreground = Brushes.Green;
         }
     }
 }
